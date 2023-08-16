c8962b3f90bd47c45328554668889d99cd6e272f
Andrew Olsen <andrew.olsen@koordinates.com>
2023-08-16 14:02:44 +1200

    Move loading screen out of Zinc.java

---
+++ zinc/core/src/main/java/zinc/core/LoadingScreen.java
@@ -0,0 +1,56 @@
+package zinc.core;
+
+public class LoadingScreen {
+
+  private boolean fontLoaded;
+  private boolean isFinished;
+
+  public static void startLoading() {
+    MenuGfx.startLoading();
+    Font.startLoading();
+    Tiles.startLoading();
+    Sprites.startLoading();
+    Sounds.startLoading();
+  }
+
+  public void update() {
+    if (!fontLoaded && Font.RAW.isLoaded()) {
+      Font.finishLoading();
+      fontLoaded = true;
+
+    } else if (Image.LOAD_TRACKER.isLoaded() &&
+               /*Sound.LOAD_TRACKER.isLoaded() &&*/
+               (Sounds.music ? Sound.MUSIC_LOAD_TRACKER.isLoaded() : true)) {
+
+      MenuGfx.finishLoading();
+      Tiles.finishLoading();
+      Sprites.finishLoading();
+      Sounds.finishLoading();
+      isFinished = true;
+    }
+  }
+
+  public void draw(Surface surface) {
+    if (!fontLoaded) {
+      surface.clear(1, 1, 1, 1);
+      return;
+    }
+
+    Font.WHITE.singleLine(surface, "Images: " + Image.LOAD_TRACKER.text(), 8 * 3, 8 * 3);
+    Font.WHITE.singleLine(surface, "Sounds: " + Sound.LOAD_TRACKER.text(), 8 * 3, 8 * 5);
+    Font.WHITE.singleLine(surface, "Music: " + Sound.MUSIC_LOAD_TRACKER.text(), 8 * 3, 8 * 7);
+
+    if (isFinished) {
+      Font.WHITE.singleLine(surface, "[Click to begin]", 8 * 3, 8 * 9);
+    }
+  }
+
+  public boolean isFinished() {
+    return isFinished;
+  }
+
+  public void finishClick() {
+    assert isFinished;
+    Sounds.FANFARE.play();
+  }
+}
--- zinc/core/src/main/java/zinc/core/Zinc.java
+++ zinc/core/src/main/java/zinc/core/Zinc.java
@@ -16,6 +16,8 @@ public class Zinc extends SceneGame {
   private Surface surface;
   private int updatedAtMs;
 
+  private LoadingScreen loadingScreen;
+
   private boolean fontLoaded = false;
   private boolean loadingFinished = false;
   private boolean clicked = false;
@@ -26,11 +28,8 @@ public class Zinc extends SceneGame {
     surface = new Surface(viewSurf);
     surface.clipFactor = platform.raw.graphics().scale().factor * platform.zoom;
 
-    MenuGfx.startLoading();
-    Font.startLoading();
-    Tiles.startLoading();
-    Sprites.startLoading();
-    Sounds.startLoading();
+    loadingScreen = new LoadingScreen();
+    LoadingScreen.startLoading();
 
     plat.input().keyboardEnabled = true;
     plat.input().keyboardEvents.connect(keySlot);
@@ -42,27 +41,11 @@ public class Zinc extends SceneGame {
   @Override
   public void update(Clock clock) {
     updatedAtMs = Platform.INSTANCE.raw.tick();
-    if (loadingFinished && clicked) {
-      if (!musicFired) {
-        Sounds.FANFARE.play();
-        musicFired = true;
-      }
-    } else if (loadingFinished) {
-      // Nothing required.
-    } else if (!fontLoaded && Font.RAW.isLoaded()) {
-      Font.finishLoading();
-      fontLoaded = true;
-
-    } else if (Image.LOAD_TRACKER.isLoaded() &&
-               /*Sound.LOAD_TRACKER.isLoaded() &&*/
-               (Sounds.music ? Sound.MUSIC_LOAD_TRACKER.isLoaded() : true)) {
-
-      MenuGfx.finishLoading();
-      Tiles.finishLoading();
-      Sprites.finishLoading();
-      Sounds.finishLoading();
-      loadingFinished = true;
+    if (loadingScreen != null) {
+      loadingScreen.update();
+      return;
     }
+    // Main game loop.
   }
 
   @Override
@@ -74,37 +57,30 @@ public class Zinc extends SceneGame {
     surface.clear(0, 0, 0, 1);
     
     surface.scale(Platform.INSTANCE.zoom, Platform.INSTANCE.zoom);
-    
+
     try {
-      if (loadingFinished && clicked) {
-        surface.draw(MenuGfx.TITLE, 0, 0);
-        if (ControllerHub.INSTANCE != null) {
-          int numControllers = ControllerHub.INSTANCE.getControllerCount();
-          Font.WHITE.singleLine(surface, "Controllers found: " + numControllers, 8 * 3, 8 * 20);
-          if (numControllers > 0) {
-            Font.WHITE.singleLine(surface, "Controller X: " + ControllerHub.INSTANCE.X(), 8 * 3, 8 * 21);
-            Font.WHITE.singleLine(surface, "Controller Y: " + ControllerHub.INSTANCE.Y(), 8 * 3, 8 * 22);
-          }
-        }
-      } else if (fontLoaded) {
-        surface.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0xff000000);
-        renderLoadingProgress(surface);
-      } else {
-        viewSurf.clear(1, 1, 1, 1);
-      }
-      
+      draw(surface);
     } finally {
       surface.end();
       surface.restoreTx();
     }
   }
 
-  private void renderLoadingProgress(Surface surface) {
-    Font.WHITE.singleLine(surface, "Images: " + Image.LOAD_TRACKER.text(), 8 * 3, 8 * 3);
-    Font.WHITE.singleLine(surface, "Sounds: " + Sound.LOAD_TRACKER.text(), 8 * 3, 8 * 5);
-    Font.WHITE.singleLine(surface, "Music: " + Sound.MUSIC_LOAD_TRACKER.text(), 8 * 3, 8 * 7);
-    if (loadingFinished) {
-      Font.WHITE.singleLine(surface, "[Click to begin]", 8 * 3, 8 * 9);
+  public void draw(Surface surface) {
+    if (loadingScreen != null) {
+      loadingScreen.draw(surface);
+      return;
+    }
+
+    surface.draw(MenuGfx.TITLE, 0, 0);
+
+    if (ControllerHub.INSTANCE != null) {
+      int numControllers = ControllerHub.INSTANCE.getControllerCount();
+      Font.WHITE.singleLine(surface, "Controllers found: " + numControllers, 8 * 3, 8 * 20);
+      if (numControllers > 0) {
+        Font.WHITE.singleLine(surface, "Controller X: " + ControllerHub.INSTANCE.X(), 8 * 3, 8 * 21);
+        Font.WHITE.singleLine(surface, "Controller Y: " + ControllerHub.INSTANCE.Y(), 8 * 3, 8 * 22);
+      }
     }
   }
   
@@ -119,8 +95,9 @@ public class Zinc extends SceneGame {
   
   private Slot<Mouse.Event> mouseSlot = new Slot<Mouse.Event>() {
     public void onEmit(Mouse.Event e) {
-      if (e instanceof Mouse.ButtonEvent && loadingFinished) {
-        clicked = true;
+      if (e instanceof Mouse.ButtonEvent && loadingScreen != null && loadingScreen.isFinished()) {
+        loadingScreen.finishClick();
+        loadingScreen = null;
       }
 
 	    /*if (e instanceof Mouse.ButtonEvent) {
